a87b617f7ced1aead7c6ca62b544d5cbc0245efb
Alan-Miller <alan.ashman@gmail.com>
2016-10-25 09:43:26 -0600

    tried some things with StatusBar

--- snapchap/www/app/app.js
+++ snapchap/www/app/app.js
@@ -123,8 +123,8 @@ angular.module('snapchat', ['ionic', 'ngCordova'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the form inputs)
     // if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    //   cordova.plugins.Keyboard.disableScroll(true);
+      // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // cordova.plugins.Keyboard.disableScroll(true);
     // }
     ionic.Platform.fullScreen();
     // if (window.StatusBar) {
