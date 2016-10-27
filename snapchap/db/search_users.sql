5272235e3ebedc513d3b3d433f5594e248e60b7e
Jon Myrick <jonmyrick7401@gmail.com>
2016-10-27 12:01:55 -0600

    search only brings results who are not friends nor friend requested

--- snapchap/db/search_users.sql
+++ snapchap/db/search_users.sql
@@ -1,1 +1,9 @@
-SELECT username, id, first_name, last_name FROM users WHERE username LIKE $1
+SELECT id, username, first_name, last_name from users
+WHERE id IN 
+(SELECT id
+FROM users  
+WHERE username LIKE $1
+except 
+select friendships.initiator from friendships
+except 
+select friendships.acceptor from friendships)
