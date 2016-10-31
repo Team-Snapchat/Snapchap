e9ab0ae5480a035e58cc429bf7727d21351d5755
Gustavo Ribeiro <Guster@Gustavos-MacBook-Pro.local>
2016-10-31 15:54:02 -0600

    set up amazong s3 to store the images

--- snapchap/serverControl.js
+++ snapchap/serverControl.js
@@ -6,6 +6,8 @@ var io = server.io;
 var config = server.config;
 var db = app.get('db');
 
+
+
 var whosLoggedIn = {}
 
 io.on('connection', function(socket){
@@ -28,7 +30,7 @@ io.on('connection', function(socket){
         delete whosLoggedIn[key];
         console.log(whosLoggedIn)
       }
-    }    
+    }
 })
 
 })
